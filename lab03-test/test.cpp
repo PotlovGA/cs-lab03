db0d402fd4c6f2d5a7a362cf06b2ca04be3b5c3e
PotlovGA <PotlovGA@example.com>
2020-05-26 10:57:55 +0300

    code: input_numbers

--- lab03-test/test.cpp
+++ lab03-test/test.cpp
@@ -5,52 +5,61 @@ const double MAX = 0;
 const double MIN = 0;
 
 void
-test_positive() {
+test_positive()
+{
     double min = MIN;
     double max = MAX;
     find_minmax({1, 2, 3}, min, max);
     assert(min == 1);
     assert(max == 3);
 }
-void test_negative() {
+void test_negative()
+{
     double min = MIN;
     double max = MAX;
     find_minmax({-1, -2, -3}, min, max);
     assert(min == -3);
     assert(max == -1);
 }
-void test_equal() {
+void test_equal()
+{
     double min = MIN;
     double max = MAX;
     find_minmax({1, 1, 1}, min, max);
     assert (min == 1);
     assert (max == 1);
 }
-void test_sole() {
+void test_sole()
+{
     double min = MIN;
     double max = MAX;
     find_minmax({1}, min, max);
     assert (min == 1);
     assert (max == 1);
 }
-void test_null() {
+void test_null()
+{
     double min = MIN;
     double max = MAX;
     find_minmax({}, min, max);
     assert (min == MIN);
     assert (max == MAX);
 }
-void test_scaling_positive() {
+void test_scaling_positive()
+{
     assert(!scaling_needed({100}, 10, 50));
 }
-void test_scaling_negative() {
+void test_scaling_negative()
+{
     assert(scaling_needed({10}, 10, 200));
 }
-void test_scaling_null() {
+void test_scaling_null()
+{
     assert(scaling_needed({}, 10, 50) == -1);
 }
 int
-main() {
+main()
+{
     test_positive();
     test_negative();
     test_equal();
--- main.cpp
+++ main.cpp
@@ -5,12 +5,12 @@
 
 using namespace std;
 vector<double>
-input_numbers(size_t count)
+input_numbers(istream& in, size_t count)
 {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++)
     {
-        cin >> result[i];
+        in >> result[i];
     }
     return result;
 }
@@ -21,12 +21,13 @@ int main()
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
-    if (!number_count) {
+    if (!number_count)
+    {
         cerr << "Error: the array can't be empty";
         return 0;
     }
     cerr << "Enter numbers: ";
-    const auto numbers = input_numbers(number_count);
+    const auto numbers = input_numbers(cin, number_count);
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
