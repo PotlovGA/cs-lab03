c2390158ccee163571db594369a39fbc428392e9
PotlovGA <PotlovGA@example.com>
2020-05-28 09:01:48 +0300

    code: major and minor version output

--- main.cpp
+++ main.cpp
@@ -19,9 +19,15 @@ input_numbers(size_t count)
 int main()
 {
     DWORD info = GetVersion();
-    DWORD mask = 0b00000000'00000000'11111111'11111111;
+    DWORD mask = 0x0000ffff;
     DWORD version = info & mask;
+    DWORD platform = info >> 16;
+    DWORD mask_major = 0x0000ff;
+    DWORD version_major = version & mask_major;
+    DWORD version_minor = version >> 8;
     printf("version: %u\n", version);
+    printf("version_major: %u\n", version_major);
+    printf("version_minor: %u\n", version_minor);
     return 0;
     size_t number_count;
     cerr << "Enter number count: ";
